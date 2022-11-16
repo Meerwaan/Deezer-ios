c8c82747cb6c14df2dc879b6197e8f9c595ee884
Merwan <merwanlaouini@gmail.com>
2022-11-16 16:21:18 +0100

    search bar

---
+++ Deezer_IOS/Deezer_IOS/Artists.swift
@@ -0,0 +1,12 @@
+//
+//  Artists.swift
+//  Deezer_IOS
+//
+//  Created by Merwan Laouini on 16/11/2022.
+//
+
+import Foundation
+
+class Artist {
+    
+}
---
+++ Deezer_IOS/Deezer_IOS/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  Deezer_IOS
+//
+//  Created by Merwan Laouini on 16/11/2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
