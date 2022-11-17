07cc1ccee55edc0cdf1514c96197727cc692fb24
Yannis ROGEON <yayaro2711@gmail.com>
2022-11-17 13:08:01 +0100

    debut fonction recherche

--- Deezer_IOS/Deezer_IOS/SearchViewController.swift
+++ Deezer_IOS/Deezer_IOS/SearchViewController.swift
@@ -7,23 +7,40 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController:  UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var artist:[Artist] = []
+    @IBOutlet weak var SearchTableView: UITableView!
+    @IBOutlet weak var Search: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        SearchTableView.dataSource = self
+        SearchTableView.delegate = self
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.artist.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        /*let DetailsAlbum = storyboard?.instantiateViewController(identifier: "DetailsAlbum") as! DetailsAlbumViewController
+        DetailsAlbum.modalPresentationStyle = .fullScreen
+        DetailsAlbum.id = self.artist[indexPath.row].id
+        present(DetailsAlbum,animated: true)*/
+    }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let SearchCell = SearchTableView.dequeueReusableCell(withIdentifier: "ArtisteCell", for: indexPath)
+        
+        SearchCell.textLabel?.text = self.artist[indexPath.row].name
+        
+        return SearchCell
+    }
+    
 }
